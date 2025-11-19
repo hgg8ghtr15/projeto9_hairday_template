ce3bd5ea3fe031e46dcc02317102073ba574f6a4
Fabio Marconi <fabiolukascj@gmail.com>
2025-11-18 22:18:37 -0400

    Criadio configuração dos pacotes com, json-server, webpack, Babel, dayjs

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+// import do dayjs
+import './libs/dayjs';
+
+// import do css
+import './styles/global.css';
+import './styles/form.css';
+import './styles/schedule.css';
+
+// import dayjs from 'dayjs'; 
+// console.log('Data atual formatada:', dayjs().format('DD/MM/YYYY: HH:mm:ss'));
